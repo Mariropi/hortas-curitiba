53c5f8a39b13dfe1b75f93532fda7ba662eca5d9
Mariropi <marianarodriguespius@gmail.com>
2026-01-10 23:21:22 -0300

    Refactor locais array and improve formatting

--- script.js
+++ script.js
@@ -6,45 +6,87 @@ document.addEventListener("DOMContentLoaded", function () {
     attribution: "© OpenStreetMap"
   }).addTo(map);
 
+    const locais = [
 
-  const locais = [
     {
       nome: "Fazenda Urbana Cajuru",
+      tipo: "fazenda",
       lat: -25.4422,
       lng: -49.2316,
-      endereco: "Av. Prefeito Maurício Fruet, 1880 – Cajuru",
-      tipo: "fazenda"
+      endereco: "Av. Prefeito Maurício Fruet, 1880 – Cajuru"
     },
     {
+      nome: "Fazenda Urbana CIC",
+      tipo: "fazenda",
+      lat: -25.4900288,
+      lng: -49.3539665,
+      endereco: "Rua Maria Lúcia Locher Athayde, 7974 – São Miguel"
+    },
+    {
+      nome: "Fazenda Urbana Tatuquara",
+      tipo: "fazenda",
+      lat: -25.5877,
+      lng: -49.3482,
+      endereco: "Rua Olivardo Konoroski Bueno, 177 – Tatuquara"
+    },
+
+    {
       nome: "Horta Projeto Oásis",
+      tipo: "horta",
       lat: -25.5203,
       lng: -49.2569,
-      endereco: "Rua Padre Stanislau Trzebialowski, 252 – Alto Boqueirão",
-      tipo: "horta"
+      endereco: "Rua Padre Stanislau Trzebialowski, 252 – Alto Boqueirão"
     },
     {
       nome: "Horta Comunitária Cristo Rei",
+      tipo: "horta",
       lat: -25.4359,
       lng: -49.2413,
-      endereco: "R. Roberto Cichon, 183 – Cristo Rei",
-      tipo: "horta"
+      endereco: "R. Roberto Cichon, 183 – Cristo Rei"
     },
     {
       nome: "Horta Maria Angélica",
+      tipo: "horta",
       lat: -25.5385,
       lng: -49.2958,
-      endereco: "Rua Monte das Oliveiras, 260 – Pinheirinho",
-      tipo: "horta"
+      endereco: "Rua Monte das Oliveiras, 260 – Pinheirinho"
+    },
+    {
+      nome: "Horta Comunitária Amigos da Fazendinha",
+      tipo: "horta",
+      lat: -25.4914,
+      lng: -49.3283,
+      endereco: "R. Afrânio Peixoto, 330 – Fazendinha"
+    },
+    {
+      nome: "Horta Comunitária Uma Nova Curitiba",
+      tipo: "horta",
+      lat: -25.4280,
+      lng: -49.3606,
+      endereco: "Rua Olívia G. Freitas, 471 – Orleans"
+    },
+    {
+      nome: "Horta Comunitária Dembinski II",
+      tipo: "horta",
+      lat: -25.5006,
+      lng: -49.3554,
+      endereco: "R. Rio do Sul, em frente ao nº 2290 – CIC"
+    },
+    {
+      nome: "Horta Comunitária do Jacu",
+      tipo: "horta",
+      lat: -25.4079,
+      lng: -49.2708,
+      endereco: "Rua Ângelo Zeni, em frente ao nº 56 – Bom Retiro"
     }
+
   ];
 
- 
   locais.forEach(local => {
     L.marker([local.lat, local.lng])
       .addTo(map)
       .bindPopup(`<strong>${local.nome}</strong><br>${local.endereco}`);
   });
-
 
   const cards = document.getElementById("cards");
 
@@ -69,14 +111,13 @@ document.addEventListener("DOMContentLoaded", function () {
     cards.appendChild(div);
   });
 
-  /* FUNÇÕES GLOBAIS */
   window.verNoMapa = function (lat, lng) {
     map.setView([lat, lng], 16);
   };
 
   window.buscarEndereco = function () {
     alert(
-      "Procure o coordenador da horta mais próxima (associações de moradores),\n" +
+      "Procure o coordenador da horta mais próxima (via associações de moradores),\n" +
       "ou entre em contato com a Prefeitura pela Central 156 ou\n" +
       "agriculturaurbana@curitiba.pr.gov.br"
     );
@@ -90,5 +131,4 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("modalFazenda").style.display = "none";
   };
 
-}); 
-
+});
